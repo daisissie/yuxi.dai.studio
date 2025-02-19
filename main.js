1aedca0deebfcacad2cfa1a80a5aa45dcfa2831c
Yuxi Dai <dai.yuxi1112@gmail.com>
2025-02-19 11:09:07 -0500

    setup

---
+++ main.js
@@ -0,0 +1,5 @@
+// This JavaScript file can be used to add interactivity.
+// For now, we'll just log a message to the console when the page loads.
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript loaded!");
+  });
